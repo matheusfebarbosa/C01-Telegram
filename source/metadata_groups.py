054a1ef8fbcd7751175200debaee0b6827a02d1d
Philipe Melo <melophilipe@gmail.com>
2020-12-07 03:05:46 -0300

    Atualizando coleta de metadados dos grupos

--- source/metadata_groups.py
+++ source/metadata_groups.py
@@ -7,6 +7,8 @@ import json
 import argparse
 import traceback
 import datetime
+import random
+import time
 
 class GroupMetadataCollector():
     """
@@ -51,6 +53,8 @@ class GroupMetadataCollector():
         self.group_blacklist       = args_dict["group_blacklist"]
         self.api_id                = args_dict["api_id"]
         self.api_hash              = args_dict["api_hash"]
+        self.profile_pic           = args_dict["profile_pic"]
+        self.profiles              = args_dict["profiles"]
 
     async def run(self):
         """
@@ -69,31 +73,20 @@ class GroupMetadataCollector():
         pathlib.Path(os.path.join(new_folder, "profile_pics")).mkdir(parents=True, exist_ok=True)
 
         async with TelegramClient('/data/collector_local', self.api_id, self.api_hash) as client:
+            
+            print("Login na API do Telegram realizado com sucesso. Coletando grupos")
             async for dialog in client.iter_dialogs():
-                if ((not dialog.is_group) or dialog.title in self.group_blacklist or
-                        str(abs(dialog.id)) in self.group_blacklist):
-                    continue
+                WAIT_TIME = random.randint(10, 25)
+                await asyncio.sleep(WAIT_TIME)
+                if (
+                    (not (dialog.is_group or dialog.is_channel) ) or 
+                    (dialog.title in self.group_blacklist) or
+                    str(abs(dialog.id)) in self.group_blacklist):
+                        continue
+                
                 group = {}
 
                 creator_id = None
-
-                participants = list()
-                async for member in client.iter_participants(dialog):
-                    user = dict()
-                    #TODO: changed some stuff here.
-                    user['id'] = member.id
-                    user['username'] = member.username
-                    user['first_name'] = member.first_name
-                    user['last_name'] = member.last_name
-                    user['number'] = member.phone
-                    user['isBot'] = member.bot
-                    user['profile_pic'] = os.path.join(new_folder, "profile_pics", str(member.id) + '.jpg')
-
-                    if not os.path.isfile(user['profile_pic']):
-                        await client.download_profile_photo(member, user['profile_pic'])
-
-                    participants.append(user)
-
                 #TODO: check what kind is and dont know how to get creator
                 group['group_id'] = dialog.entity.id
                 # group['creator'] = creator
@@ -102,13 +95,53 @@ class GroupMetadataCollector():
                 group['creation']['creation_date'] = dialog.entity.date.strftime('%Y-%m-%d %H:%M:%S')
                 group['creation']['creation_timestamp'] = int(datetime.datetime.timestamp(dialog.entity.date))
                 group['title'] = dialog.entity.title
+                group['collection_date'] = now.strftime('%Y-%m-%d')
+                
+                if dialog.is_channel:  groupType = "channel"
+                if dialog.is_group:    groupType = "group"
+                group['group_type'] = groupType
+               
+                print(group)
+                
+                participants = list()
+                if dialog.is_group and self.profiles:
+                    async for member in client.iter_participants(dialog):
+                        user = dict()
+                        #TODO: changed some stuff here.
+                        user['id'] = member.id
+                        user['username'] = member.username
+                        user['first_name'] = member.first_name
+                        user['last_name'] = member.last_name
+                        user['number'] = member.phone
+                        user['isBot'] = member.bot
+                        user['profile_pic'] = None
+                            
+                        
+                        if self.profile_pic:
+                            user['profile_pic'] = os.path.join(new_folder, "profile_pics", str(member.id) + '.jpg')
+                            if not os.path.isfile(user['profile_pic']):
+                                try:
+                                    await client.download_profile_photo(member, user['profile_pic'])
+                                except:
+                                    print('Error downloading profile picture for', member.id, member.username)
+                            
+                        participants.append(user)
                 group['members'] = participants
-
+                
                 filename = os.path.join(new_folder, 'grupos.json')
                 with open(filename, 'a') as json_file:
                     json.dump(group, json_file)
                     print('', file=json_file)
-
+                
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -116,6 +149,14 @@ async def main():
     parser.add_argument("--group_blacklist", nargs="+",
                         help="Lista de ids de grupos que devem ser excluídos"
                         " da coleta", default=[])
+
+    parser.add_argument("--profile_pic",  type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="Flag para baixar ou não as fotos de perfil dos usuários ")
+                        
+    parser.add_argument("--profiles", type=str2bool, nargs='?',
+                        const=True, default=True,
+                        help="Flag para listar quem sÃo os usuários ")
 
     parser.add_argument("--api_id", type=str,
                         help="ID da API de Coleta gerado em my.telegram.org (Dado sensível)")
@@ -136,7 +177,8 @@ async def main():
                         "comando, caso eles sejam fornecidos.")
 
     args = parser.parse_args()
-
+    
+    print("Inicializando coletor de metadados")
     try:
         collector = GroupMetadataCollector(args)
         await collector.run()
